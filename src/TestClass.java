a1e56e6b25a8ba6e62ef7916e3bf6214c0ba2ebf
gtcat <gery.bieli@gmx.ch>
2016-03-26 15:14:50 +0100

    Initial commit

---
+++ src/TestClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by gbieli on 26.03.16.
+ */
+
+public class TestClass {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+
+        for(int i=0; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+}
