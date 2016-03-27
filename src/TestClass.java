3d36a6d40e81ede2e9490530d6df2bb631679a4c
gtcat <gery.bieli@gmx.ch>
2016-03-27 11:47:10 +0200

    GUI

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,3 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.util.LinkedList;
+
 /**
  * Created by gbieli on 26.03.16.
  */
@@ -9,5 +15,20 @@ public class TestClass {
         for(int i=0; i<=5; i++){
             System.out.println(i);
         }
+
+        LinkedList<String> list = new LinkedList<>();
+        list.add("bla");
+        list.add("bla2");
+
+        for(String element : list){
+            System.out.println(element.toString());
+        }
+
+        JFrame frame = new JFrame("Frame");
+        frame.setLayout(new FlowLayout());
+        JButton button = new JButton("click");
+        frame.add(button);
+        frame.setSize(400,400);
+        frame.setVisible(true);
     }
 }
